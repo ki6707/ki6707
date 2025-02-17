056204d0ed3c4ee728a144f5934c79f42d719cc0
ki6707 <chiyaanofxl@gmail.com>
2025-02-17 18:56:22 +0530

    anagrams
    anagrams

---
+++ anagrams.py
@@ -0,0 +1,10 @@
+while True:
+
+    a=str(input("enter a string1: "))
+    b=str(input("enter a string2: "))
+    if(sorted(a)==sorted(b)):
+        print("the givien words is anagram")
+    else:
+        print("the not givien words is anagram")
+
+
