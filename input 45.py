aeb7406a202e1e27de2b94632cab6fe4e37294f2
ki6707 <chiyaanofxl@gmail.com>
2025-02-19 14:39:21 +0530

    number into sentance

---
+++ input 45.py
@@ -0,0 +1,23 @@
+a=str(input("enter a number: "))
+
+for i in range(len(a)):
+    if(a[i]=='1'):
+        print("one",end=" ")
+    elif(a[i]=='2'):
+        print("two",end=" ")
+    elif(a[i]=='3'):
+        print("three",end=" ")
+    elif(a[i]=='4'):
+        print("four",end=" ")
+    elif(a[i]=='5'):
+        print("five",end=" ")
+    elif(a[i]=='6'):
+        print("six",end=" ")
+    elif(a[i]=='7'):
+        print("seven",end=" ")
+    elif(a[i]=='8'):
+        print("eight",end=" ")
+    elif(a[i]=='9'):
+        print("nine",end=" ")
+    elif(a[i]=='0'):
+        print("zero",end=" ")    
