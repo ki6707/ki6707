9c8fa8a36b4cad6e1a69a57b2c289c324da1a6c1
ki6707 <chiyaanofxl@gmail.com>
2025-04-19 13:10:55 +0530

    tens number
    tens number

---
+++ tens number.py
@@ -0,0 +1,18 @@
+number=1
+while(number<=100):
+    if(number%5==0):
+        if(number%2==0):
+            print("THE VALUES: ",number)
+    number=number+1
+
+#output:
+# THE VALUES:  10
+# THE VALUES:  20      
+# THE VALUES:  30      
+# THE VALUES:  40      
+# THE VALUES:  50      
+# THE VALUES:  60      
+# THE VALUES:  70      
+# THE VALUES:  80      
+# THE VALUES:  90      
+# THE VALUES:  100  
